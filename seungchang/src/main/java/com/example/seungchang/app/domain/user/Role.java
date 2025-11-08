11de550c84cb951a54385a1249a48cbfbf607a7d
Lee Seungchang <jjekrrq01@naver.com>
2025-11-08 22:38:34 +0900

    feat : implement spring security & synchronize bossname with Authenticated UserName

---
+++ seungchang/src/main/java/com/example/seungchang/app/domain/user/Role.java
@@ -0,0 +1,6 @@
+package com.example.seungchang.app.domain.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ seungchang/src/main/java/com/example/seungchang/global/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.example.seungchang.global.config;
+
+import com.example.seungchang.global.jwt.JwtFilter;
+import com.example.seungchang.global.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final TokenProvider tokenProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/restaurant/**").permitAll() // 조회는 누구나
+                        .anyRequest().authenticated() // 나머지는 인증 필요 (create/update/delete 등)
+                )
+                .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
